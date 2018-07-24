b691475fb948a6f04a629f2cc525352f00eae5dd
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-24 13:29:02 -0400

    initial commit

---
+++ src/com/company/SpecialCharacterProgram.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class SpecialCharacterProgram {
+    public static void main (String [] args){
+      //  String message1, message2;
+
+      //  message1 = \/\/\/\/\/\r\t\b
+         //       message2 = ";
+
+      //  System.out.println(message1 + message2);
+    }
+
+
+}
---
+++ src/com/company/SpecialCharactersSoution.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class SpecialCharactersSoution {
+    public static void main (String [] args){
+        String message1, message2;
+
+        message1 = "\\/\\/\\/\\/\\/\\r\\t\\b";
+                message2 = "\"";
+
+        System.out.println("message 1="+message1 +"\nmessage 2 ="+ message2);
+    }
+
+}
